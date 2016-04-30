2b93a9cdf03102c883cddb80b62d5711739a2972
ababup1192 <ababup1192@gmail.com>
2016-05-01 00:16:30 +0900

    Implement immutable queue

--- 03-ElementaryDataStructures/B_Queue/Main.scala
+++ 03-ElementaryDataStructures/B_Queue/Main.scala
@@ -1,48 +1,56 @@
-import scala.collection.mutable.ArrayBuffer
 import scala.io.StdIn
+import scala.annotation.tailrec
+
+case class Process(name: String, time: Int){
+  override def toString(): String = s"$name $time"
+}
 
 object Main extends App {
+  val Array(_, quantum) = StdIn.readLine().split(" ").map(_.toInt)
 
-  val Array(n,  quantum) = StdIn.readLine().split(" ").map(_.toInt)
-  val queue = Queue(quantum)
-
-  Iterator.continually(StdIn.readLine()).takeWhile(_ != null).foreach {
-    _.split(" ") match {
-      case Array(pName: String,  time: String) => queue.enqueue(Data(pName,  time.toInt))
+  val queue = Iterator.continually(StdIn.readLine()).takeWhile(_ != null).foldLeft(Queue.empty) {
+    (queue, line) => {
+      line.split(" ") match {
+        case Array(name: String,time: String) => queue.enqueue(Process(name, time.toInt))
+      }
     }
   }
 
-  Iterator.continually {
-    val data = queue.dequeue()
-    if (data.time == 0) {
-      println(s"${data.pName} ${queue.currentTime}")
+  @tailrec
+  def loop(queue: Queue, out: Vector[Process] = Vector(), curTime: Int = 0): Vector[Process] = {
+    if(queue.isEmpty) out
+    else {
+      val(process, nextQueue) = queue.dequeue
+      if(process.time > quantum){
+        loop(nextQueue.enqueue(process.copy(time = process.time - quantum)), out, curTime + quantum)
+      }else{
+        loop(
+          nextQueue,
+          out :+ process.copy(time = curTime + process.time),
+          curTime + process.time
+        )
+      }
     }
-    queue.isEmpty
-  } forall (!_)
+  }
+
+  println(loop(queue).mkString("\n"))
 }
 
-case class Queue(quantum: Int) {
-  var currentTime = 0
-  private[this] val queue = ArrayBuffer.empty[Data]
+class Queue private(val in: List[Process], val out: List[Process]){
+  def enqueue(elem: Process) = new Queue(elem :: in, out)
 
-  def enqueue(data: Data): Unit = {
-    queue.append(data)
+  def dequeue: (Process, Queue) = out match {
+    case Nil if !in.isEmpty => val rev = in.reverse ; (rev.head, new Queue(Nil, rev.tail))
+    case x :: xs            => (x, new Queue(in, xs))
+    case _                  => throw new NoSuchElementException("dequeue on empty queue")
   }
 
-  def dequeue(): Data = {
-    val data = queue.remove(0)
-    val time = Math.min(quantum,  data.time)
-    currentTime += time
-    val newData = Data(data.pName,  data.time - time)
-    if (newData.time != 0) {
-      queue.append(newData)
-    }
-    newData
-  }
+  def isEmpty: Boolean = in.isEmpty && out.isEmpty
 
-  def isEmpty: Boolean = {
-    queue.isEmpty
-  }
+  override def toString() = in.toString
 }
 
-case class Data(pName: String,  time: Int)
+object Queue {
+  def empty: Queue = new Queue(Nil, Nil)
+  def apply(xs: Process*): Queue = new Queue(Nil, xs.toList)
+}
